3923c8303e088d866ef31a59bdfaf1babc6d7773
Mikkel Pavia <mikkelpaviahansen@gmail.com>
2022-11-21 15:29:16 +0100

    Rewritten to use ULP. Uses switch case, same principles as before

--- src/Semesterprojekt.cpp
+++ src/Semesterprojekt.cpp
@@ -12,169 +12,174 @@
  *
  * Notifications: https://www.hackster.io/gusgonnet/add-push-notifications-to-your-hardware-41fa5e
  */
-#include "..\lib\RTClibraryDS3231_DL\src\RTClibraryDS3231_DL.h"
 #include "WiFiCredentials.h"
-//#define DBG
-
 void setup();
 void loop();
-void myHandler(const char *event, const char *data);
+void subHandler(const char *event, const char *data);
 void forceSleep();
-#line 13 "c:/particle_workspace/Semesterprojekt/src/Semesterprojekt.ino"
-void interruptHandler();
+#line 10 "c:/particle_workspace/Semesterprojekt/src/Semesterprojekt.ino"
+#define DBG
 
-#define LED D7
-#define SENSOR A3
-#define INT_INPUT D5
+#define EXTENDED_INFO 1
 #define GET_WEATHER_DATA "hook-response/GET_WEATHER_DATA"
-#define VBAT_ADC A2
-#define VBAT_CTRL D2
+
+// Thresholds
 #define SENSOR_DRY 3400
 #define SENSOR_WET 1400
-#define LOW_MOISTURE 50
 #define SIGNIFICANT_PERCIPITATION 2.0
-#define EXTENDED_INFO 1
-//#define DS3231 // Uncomment or delete to use ULP
-#define GOOD_MOISTURE 65 // Moisture content in percent
+#define LOW_MOISTURE 65
 #define VBAT_LOW 3.5
 
-RTC_DS3231 rtc;
+// Pins
+#define SENSOR A3
+#define VBAT_ADC A2
+#define VBAT_CTRL D2
+
+// Constants
+#define MAX_RUNTIME 45000
+#define ADC_RES (3.3 / 4096);
+#define VOLTAGE_DIVIDER_SCALE_FACTOR 5.45
+
 LEDStatus statusOff;
-LEDStatus blinkRGB(RGB_COLOR_RED, LED_PATTERN_BLINK, LED_SPEED_NORMAL, LED_PRIORITY_CRITICAL);
 SystemSleepConfiguration config;
 
-Timer timeoutTimer(45000, forceSleep, true);
+Timer timeoutTimer(MAX_RUNTIME, forceSleep, false);
 SYSTEM_MODE(AUTOMATIC); // Call setup() before cloud connection is up
 SYSTEM_THREAD(ENABLED);
-
+#ifdef DBG
+unsigned int totalTime = 0;
+char str[80];
+SerialLogHandler logHandler(115200);
+const char *STATE_NAMES[8] = {
+    "COLD_START",
+    "READ_SENSORS",
+    "RETURN_FROM_SLEEP",
+    "CONNECTED",
+    "AWAIT_RESPONSE",
+    "HANDLE_DATA",
+    "FORCE_SLEEP",
+    "SLEEP"};
+#endif
 unsigned int timeStart = 0;
 unsigned int alarmFired = 0;
 float futurePercipitation;
-float VBAT_ACTUAL;
+double VBAT_ACTUAL;
 int soilMoisture;
 int VBAT;
-bool state = false;
 volatile bool gotWeatherData = false;
-String requestString = "\r";
+typedef enum STATES
+{
+    COLD_START,
+    READ_SENSORS,
+    RETURN_FROM_SLEEP,
+    CONNECTED,
+    AWAIT_RESPONSE,
+    HANDLE_DATA,
+    FORCE_SLEEP,
+    SLEEP
 
-pin_t PINS[] = {D3, D6, D9};
+} state_t;
+volatile state_t currentState = COLD_START, lastState;
+pin_t PINS[] = {D3, D4, D5, D6, D7, D8, D9};
 
 void setup()
 {
-    // Set WiFi credentials to office WiFi
-    WiFi.setCredentials(WiFiSSID, WiFiPW);
-#ifdef DBG
-    timeStart = millis();
-#endif
-    timeoutTimer.start();
-    gotWeatherData = false;
-    statusOff.setPriority(LED_PRIORITY_CRITICAL);
-    statusOff.setActive();
-    statusOff.off();
-    // Set unused pins as output
-    for (unsigned int i = 0; i < sizeof(PINS) / sizeof(PINS[0]); i++)
-    {
-        pinMode(PINS[i], OUTPUT);
-        digitalWrite(PINS[i], LOW);
-    }
-    // Configure interrupt pin
-    pinMode(INT_INPUT, INPUT);
-
-    // Control pin for VBAT
-    pinMode(VBAT_CTRL, OUTPUT);
-
-    // Setup sensors
-    pinMode(SENSOR, INPUT);
-    pinMode(VBAT_ADC, INPUT);
-
-#ifdef DS3231
-#ifdef DBG
-    blinkRGB.setActive();
-#endif
-    while (!rtc.begin())
-        ;
-    while (rtc.lostPower())
-    {
-
-        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-        rtc.disable32K();
-        rtc.clearAlarm(1);
-        rtc.clearAlarm(2);
-        rtc.writeSqwPinMode(DS3231_OFF);
-        rtc.disableAlarm(1);
-        rtc.disableAlarm(2);
-        DateTime dt = DateTime((F(__DATE__), "16:00:00"));
-
-        while (!rtc.setAlarm1(dt, DS3231_A1_Hour)) // Debug: DS3231_A1_Second
-            ;
-        dt = DateTime((F(__DATE__), "08:00:00"));
-        while (!rtc.setAlarm2(dt, DS3231_A2_Hour))
-            ;
-    }
-#ifdef DBG
-    blinkRGB.setActive(false);
-#endif
-    if (rtc.alarmFired(1) || rtc.alarmFired(2))
-    {
-        rtc.clearAlarm(1);
-        rtc.clearAlarm(2);
-    }
-    config.mode(SystemSleepMode::HIBERNATE);
-    config.gpio(INT_INPUT, FALLING);
-#else
-    config.mode(SystemSleepMode::ULTRA_LOW_POWER);
-    config.duration(15min); // 480 min for 8 hours
-#endif
-
-    // Sensor read
-    soilMoisture = analogRead(SENSOR);
-    soilMoisture = map(soilMoisture, SENSOR_DRY, SENSOR_WET, 0, 100);
-
-    // Read VBAT
-    digitalWrite(VBAT_CTRL, HIGH);
-    VBAT = analogRead(VBAT_ADC);
-    digitalWrite(VBAT_CTRL, LOW);
-
-    // Calculate voltage
-    float ADC_RES = 3.3 / 4096;
-    VBAT_ACTUAL = VBAT * ADC_RES * 5.6;
-
-    // If soil moisture is high, go to sleep immediately, don't even wait for cloud connection
-    if (soilMoisture >= GOOD_MOISTURE && VBAT_ACTUAL > VBAT_LOW)
-        System.sleep(config);
-
-    // Wait for cloud connection
-    while (!Particle.connected())
-        ;
-
-    // Subscribe to weather data hook
-    bool subRes;
-    subRes = Particle.subscribe(GET_WEATHER_DATA, myHandler, MY_DEVICES);
-    if (!subRes)
-    {
-    }
-    bool res;
-    res = Particle.publish("GET_WEATHER_DATA", PRIVATE);
-    if (!res)
-    {
-    }
 }
 
 void loop()
 {
-    if (gotWeatherData)
-    {
 #ifdef DBG
-        unsigned int totalTime = millis() - timeStart;
-        char str[80];
-        sprintf(str, "P%.1f, SM%d, UP%u", futurePercipitation, soilMoisture, totalTime);
-        Particle.publish("DATA", str, PRIVATE);
+    if (currentState != lastState)
+    {
+        Log.info("\n\t\tSTATE: " + String(STATE_NAMES[currentState]) + "\n");
+        if (Particle.connected())
+            Particle.publish("STATE", STATE_NAMES[currentState], PRIVATE);
+    }
 #endif
-        // Notify user to water plants
+    switch (currentState)
+    {
+    case COLD_START:
+        // Set WiFi credentials to office WiFi
+        WiFi.setCredentials(WiFiSSID, WiFiPW);
+#ifdef DBG
+        timeStart = millis();
+#endif
+        timeoutTimer.reset();
+        statusOff.setPriority(LED_PRIORITY_CRITICAL);
+        statusOff.setActive();
+        statusOff.off();
+        // Set unused pins as output
+        for (unsigned int i = 0; i < sizeof(PINS) / sizeof(PINS[0]); i++)
+        {
+            pinMode(PINS[i], OUTPUT);
+            digitalWrite(PINS[i], LOW);
+        }
+
+        // Control pin for VBAT
+        pinMode(VBAT_CTRL, OUTPUT);
+
+        // Setup sensors
+        pinMode(SENSOR, INPUT);
+        pinMode(VBAT_ADC, INPUT);
+
+        config.mode(SystemSleepMode::ULTRA_LOW_POWER);
+        config.duration(1min); // 480 min for 8 hours
+        currentState = READ_SENSORS;
+        break;
+
+    case READ_SENSORS:
+        // Sensor read
+        soilMoisture = analogRead(SENSOR);
+        soilMoisture = map(soilMoisture, SENSOR_DRY, SENSOR_WET, 0, 100);
+        // Read VBAT
+        digitalWrite(VBAT_CTRL, HIGH);
+        VBAT = analogRead(VBAT_ADC);
+        digitalWrite(VBAT_CTRL, LOW);
+
+        // Calculate voltage
+        VBAT_ACTUAL = VBAT * ADC_RES;
+        VBAT_ACTUAL *= VOLTAGE_DIVIDER_SCALE_FACTOR;
+        // If soil moisture is high, go to sleep immediately, don't even wait for cloud connection
+        if (soilMoisture >= LOW_MOISTURE && VBAT_ACTUAL > VBAT_LOW)
+            currentState = SLEEP;
+        else
+            currentState = CONNECTED;
+        break;
+
+    case CONNECTED:
+        // Wait for cloud connection
+        while (!Particle.connected())
+            ;
+
+        // Subscribe to weather data hook
+        bool subRes;
+        subRes = Particle.subscribe(GET_WEATHER_DATA, subHandler, MY_DEVICES);
+        if (!subRes)
+        {
+        }
+        bool res;
+        res = Particle.publish("GET_WEATHER_DATA", PRIVATE);
+        if (!res)
+        {
+        }
+        currentState = AWAIT_RESPONSE;
+        break;
+
+    case AWAIT_RESPONSE:
+        // IDLE?
+        break;
+
+    case HANDLE_DATA:
+#ifdef DBG
+        totalTime = millis() - timeStart;
+        sprintf(str, "%s P%.1f, SM%d, UP%u B%.2lf", Time.timeStr().c_str(), futurePercipitation, soilMoisture, totalTime, VBAT_ACTUAL);
+        Particle.publish("pushbullet", str, PRIVATE);
+#else
         if (soilMoisture <= LOW_MOISTURE && futurePercipitation <= SIGNIFICANT_PERCIPITATION)
         {
             if (EXTENDED_INFO)
             {
+
                 Particle.publish("pushbullet", "Low chance of percipitation, water plants!\nR" + String(futurePercipitation, 1) + " M" + String(soilMoisture) + " B" + String(VBAT_ACTUAL, 2), PRIVATE);
             }
             else
@@ -182,22 +187,44 @@ void loop()
                 Particle.publish("pushbullet", "Low chance of percipitation, water plants!", PRIVATE);
             }
         }
+#endif
+        currentState = SLEEP;
+        break;
+
+    case SLEEP:
+        currentState = RETURN_FROM_SLEEP;
+        timeoutTimer.stop();
         System.sleep(config);
+        break;
+
+    case FORCE_SLEEP:
+        Particle.publish("FORCE_SLEEP", PRIVATE);
+        currentState = SLEEP;
+        break;
+
+    case RETURN_FROM_SLEEP:
+        timeStart = millis();
+        timeoutTimer.reset();
+        currentState = READ_SENSORS;
+        break;
+
+    default:
+        break;
     }
+    lastState = currentState;
 }
 
-void myHandler(const char *event, const char *data)
+void subHandler(const char *event, const char *data)
 {
     // Get weather data
     // https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=x&lon=y
     char d[100];
     strcpy(d, data);
     futurePercipitation = atof(data);
-    gotWeatherData = true;
+    currentState = HANDLE_DATA;
 }
 
 void forceSleep()
 {
-    Particle.publish("FORCE_SLEEP", PRIVATE);
-    System.sleep(config);
+    currentState = FORCE_SLEEP;
 }
